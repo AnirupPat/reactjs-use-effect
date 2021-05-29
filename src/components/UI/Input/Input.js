49702dcec1d8ea0959fd982277914d3ff3c7784b
Anirup Patnaik <anirup049@gmail.com>
2021-05-29 20:32:21 +0530

    Adding a new Input Component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,23 @@
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+    <label htmlFor="email">{props.label}</label>
+      <input
+        className={`${classes.input} ${props.className}`}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+};
+
+export default Input;
